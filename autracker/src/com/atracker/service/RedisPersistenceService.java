3a9094e1e451ce8d2bb0cc5b9434e6e90166271c
luobintianya <luobintianya@sina.com>
2016-10-25 18:51:42 +0800

    add for redis

--- autracker/src/com/atracker/service/RedisPersistenceService.java
+++ autracker/src/com/atracker/service/RedisPersistenceService.java
@@ -2,9 +2,10 @@ package com.atracker.service;
 
 import com.atracker.data.TrackerInfo;
 import com.atracker.persistence.PersistenceStrategy;
+import com.atracker.persistence.redis.RedisPersistenceStrategy;
 
 public class RedisPersistenceService implements PersistenceService {
-	private PersistenceStrategy redisStore;
+	private PersistenceStrategy redisStore= new RedisPersistenceStrategy();
 
 	public void persistence(TrackerInfo info) {
 		
